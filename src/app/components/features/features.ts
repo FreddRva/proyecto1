2ab4cfd39e461419f06b6c3e4228914bb34645c9
FreddRva <rvfredy9@gmail.com>
2026-03-23 09:53:41 -0500

    2

--- src/app/components/features/features.ts
+++ src/app/components/features/features.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-features',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './features.html',
   styleUrl: './features.css'
 })
--- src/app/components/why-choose-us/why-choose-us.ts
+++ src/app/components/why-choose-us/why-choose-us.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-why-choose-us',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './why-choose-us.html',
   styleUrl: './why-choose-us.css'
 })
